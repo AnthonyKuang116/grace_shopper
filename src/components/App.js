858aa55d1563a5bc020b6fc401b45fe464433294
Anthony Kuang <AnthonyKuang116@gmail.com>
2021-07-07 20:34:40 -0400

    Finished search function for both searchbar and categories

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,13 @@ const App = () => {
   const [currentUser, setCurrentUser] = useState(getCurrentUser);
   const [modalProduct, setModalProduct] = useState(null);
   const [openProduct, setOpenProduct] = useState(false);
+  const [currentSearchText, setCurrentSearchText] = useState("");
+  const [subCategory, setSubCategory] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const data = await getAllProducts();
-
+      console.log("All Products", data)
       setProducts(data);
     }
 
@@ -20,15 +22,58 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    if(!currentUser) {
+    if (!currentUser) {
       return;
     }
   }, [currentUser])
 
+  const handleSearchTextChange = (e) => {
+    const newSearchText = e.currentTarget.value;
+    setCurrentSearchText((oldSearchText) => {
+      console.log(newSearchText)
+      return newSearchText;
+    })
+  };
+
+  const handleSubCategoryChange = (event) => {
+    event.preventDefault();
+    const subCategorySearch = event.target.value;
+    setSubCategory(() => {
+      return subCategorySearch;
+    })
+  }
+
+  const filteredProducts = () => {
+    let filteredResults = products;
+
+    if (currentSearchText) {
+      let searchTextLower = currentSearchText.toLowerCase()
+      filteredResults = filteredResults.filter((item) => {
+        return item.name.toLowerCase().includes(searchTextLower)
+      })
+    }
+
+    if (subCategory.length) {
+      filteredResults = filteredResults.filter((item) => {
+        return subCategory.some(search => item.subCategory.startsWith(search))
+      })
+    }
+    return filteredResults;
+  }
+
   return (
     <div className="App">
-      <Header currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-      <Main {...{ products, setModalProduct, setOpenProduct }} />
+      <Header
+        currentUser={currentUser}
+        setCurrentUser={setCurrentUser}
+        currentSearchText={currentSearchText}
+        handleSearchTextChange={handleSearchTextChange}
+        products={products}
+        setProducts={setProducts}
+        handleSubCategoryChange={handleSubCategoryChange}
+        subCategory={subCategory}
+      />
+      <Main {...{ setModalProduct, setOpenProduct }} products={filteredProducts()} />
       <Product {...{ modalProduct, openProduct, setOpenProduct }} />
     </div>
   );
